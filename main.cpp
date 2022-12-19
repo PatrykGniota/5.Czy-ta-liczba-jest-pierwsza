fa74222aeba933df2fec0de79b17f39a97b1fc63
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2022-12-19 11:24:59 +0000

    version 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+  bool isPrime =true;
+  int i = 3;
+  if(n > 2){
+    if(n % 2 == 0 ){
+      isPrime = false;
+    }
+    else{
+      while(i * n <= n * n){
+        if(n / i == 0){
+          isPrime = false;
+        }
+        i++;
+      }
+    }    
+  }
+  return isPrime;
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+  int x = 2137;
+
+  if(isPrime(x)){
+    cout << x << " Prime\n";
+  }
+  else{
+    cout << x << " Not Prime\n";
+  }
 }
