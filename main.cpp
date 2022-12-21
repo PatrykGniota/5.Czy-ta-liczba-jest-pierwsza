095a82d0021f4bec184da64164b5fbfb36c81e28
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2022-12-21 19:15:38 +0000

    twinPrime

--- main.cpp
+++ main.cpp
@@ -1,32 +1,49 @@
 #include <iostream>
 using namespace std;
 
-void factorization(int x){
-  int d =  3;
-  cout << x << " = ";
-  while(d * d <= x){
-    while(x % 2 == 0){
-    cout << 2 << " * ";
-    x = x / 2;
+bool isPrime(int num){
+  int d = 5;
+  bool isPrime = true;
+
+  isPrime = (num > 1);
+  if(num > 2 and num % 2 == 0){
+    isPrime = false;
+  }
+  if(num > 3 and num % 3 == 0){
+    isPrime = false;
+  }
+  while(isPrime and (d*d) <= num){
+    if(num % d == 0){
+      isPrime = false;
     }
-    if(x % d == 0){
-      cout << d << " * ";
-      x = x / d;
+    else if(num %(d + 2) == 0){
+      isPrime = false;
     }
     else{
-      d += 2;
+      d = d + 6;
     }
   }
-  cout << x;
+  return isPrime;
+}
+
+void twinPrime(int num){
+  int licznik = 1;
+  int x = 5;
+  cout << 3 << " " << 5 << endl;
+  while(licznik < num){
+    if(isPrime(x) && isPrime(x + 2)){
+      cout << x << " " << x + 2 << endl;
+      licznik++;
+    }
+    x += 6;
+  }
 }
 
 int main() {
 
   int num = 0;
-  
-  cout << "num = ";
   cin >> num;
-
-  factorization(num);
+  twinPrime(num);
+  
   
 }
