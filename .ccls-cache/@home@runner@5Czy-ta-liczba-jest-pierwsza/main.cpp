3462a572164964766ccc05c32bc9bcf9bdfb311d
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2022-12-21 09:09:50 +0000

    Ćwiczenie 4 podr. str. 81

--- .ccls-cache/@home@runner@5Czy-ta-liczba-jest-pierwsza/main.cpp
+++ .ccls-cache/@home@runner@5Czy-ta-liczba-jest-pierwsza/main.cpp
@@ -4,19 +4,23 @@ using namespace std;
 int main() {
 
   int num = 0;
-  int d = 2;
+  int d = 3;
   
   cout << "num = ";
   cin >> num;
   cout << num << " = ";
 
   while(d * d <= num){
+    while(num % 2 == 0){
+      cout << 2 << " * ";
+      num = num / 2;
+    }
     if(num % d == 0){
       cout << d << " * ";
       num = num / d;
     }
     else{
-      d++;
+      d += 2;
     }
   }
   cout << num;
